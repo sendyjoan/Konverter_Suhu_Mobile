a24422dcbd0870ecbbf20b2b1d7578b42369cafa
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-09-22 21:31:46 +0700

    Melakukan Praktikum Langkah Ke-4

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the roor of your application.
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              TextFormField(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
